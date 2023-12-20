d47e3c3e85f31fe35fc3ed102209751836a0314a
KumazakiRyoha <1593713783@qq.com>
2023-12-20 23:33:55 +0900

    commit 3

---
+++ Sharing Objects/MutableInteger.java
@@ -0,0 +1,8 @@
+import net.jcip.annotations.NotThreadSafe;
+
+@NotThreadSafe
+public class MutableInteger {
+    private int value;
+    public int get() { return value; }
+    public void set(int value) { this.value = value; }
+}
