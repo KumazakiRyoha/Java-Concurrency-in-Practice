2f0c51a551afe311cb8a3b7974d5f8198ebb8378
KumazakiRyoha <1593713783@qq.com>
2024-02-07 23:39:34 +0900

    commit 14

---
+++ Composing_Objects/SingleThreadRenderer.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class SingleThreadRenderer {
+
+    void renderPage(CharSequence source) {
+        renderText(source);
+        ArrayList<ImageData> imageData = new ArrayList<ImageData>();
+        for (ImageInfo imageInfo : scanForImageInfo(source))
+            imageData.add(imageInfo.downloadImage());
+        for (ImageData data : imageData)
+            renderImage(data);
+    }
+
+
+    interface ImageData {
+    }
+
+    interface ImageInfo {
+        ImageData downloadImage();
+    }
+
+    abstract void renderText(CharSequence s);
+    abstract List<ImageInfo> scanForImageInfo(CharSequence s);
+    abstract void renderImage(ImageData i);
+
+}
